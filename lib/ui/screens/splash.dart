e7dba6599b6f5b53de610742b1bcef0533cae73b
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-10-22 08:56:19 +0600

    background & logo  splashScreen

--- lib/ui/screens/splash.dart
+++ lib/ui/screens/splash.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+import '../utilis/assets_path.dart';
+import '../widgets/screen_background.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,15 +16,17 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-
+      body: ScreenBackground(child:  Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SvgPicture.asset('assets/images/logo.svg')
+            SvgPicture.asset(AssetsPath.logoSvg,width: 120,),
+
           ],
         ),
-      ),
+      ),),
     );
   }
 }
+
+
---
+++ lib/ui/widgets/screen_background.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../utilis/assets_path.dart';
+
+class ScreenBackground extends StatelessWidget {
+  const ScreenBackground({super.key, required this.child});
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.sizeOf(context);
+    return Stack(
+
+      children: [
+        SvgPicture.asset(AssetsPath.backgroundSvg,
+          fit:BoxFit.cover,
+          height: screenSize.height,
+          width: screenSize.width,
+        ),
+        child,
+      ],
+    );
+  }
+}
